b877ea44b3d466beb7a1aca94f46a0bdc5bebb3c
Sebastien Villar <seb@height.app>
2023-02-17 15:00:34 -0500

    T-27655 / Do not create todos for already existing comments
    Closes T-27655

--- src/watcher.ts
+++ src/watcher.ts
@@ -1,20 +1,21 @@
 import updateTodos, {parseGitIgnore} from '@heightapp/update-todos';
 import createClient from 'clientHelpers/createClient';
 import getDefaultListIds from 'clientHelpers/getDefaultListIds';
-import zipObject from 'lodash/zipObject';
+import memoize from 'memoizee';
 import createThrottleQueue from 'throttled-queue';
-import {AuthenticationSession, Disposable, EventEmitter, ExtensionContext, extensions, workspace} from 'vscode';
+import {AuthenticationSession, Disposable, EventEmitter, ExtensionContext, extensions, FileType, TextDocument, Uri, workspace} from 'vscode';
 
 import path from 'path';
 
 const MAX_TASKS_PER_MINUTE = 20;
+const MAX_FOLDER_DEPTH = 30;
 
 class Watcher {
   readonly session: AuthenticationSession;
   private context: ExtensionContext;
 
   private eventEmitter = new EventEmitter<{type: 'error'; error: unknown}>();
-  private gitRepoRoots: Array<string>;
+  private gitRepoRoots: Set<string>;
   private onDidSaveTextDocumentDisposable?: Disposable;
   private throttle = createThrottleQueue(MAX_TASKS_PER_MINUTE, 60000);
 
@@ -23,8 +24,64 @@ class Watcher {
     this.context = context;
 
     const gitExtension = extensions.getExtension('vscode.git')?.exports?.getAPI(1);
-    this.gitRepoRoots = gitExtension?.repositories?.map((repo: any) => repo?.repository?.root) ?? [];
+    this.gitRepoRoots = new Set(gitExtension?.repositories?.map((repo: any) => repo?.repository?.root) ?? []);
   }
+
+  private memoizedIsRepoRoot = memoize(
+    async (uri: Uri) => {
+      if (this.gitRepoRoots.has(uri.fsPath)) {
+        // Found in git extension
+        return true;
+      }
+
+      // Check if .git exists in directory
+      try {
+        const stats = await workspace.fs.stat(Uri.joinPath(uri, '.git'));
+        return stats.type === FileType.Directory;
+      } catch (e) {
+        return false;
+      }
+    },
+    {
+      max: 1000,
+      maxAge: 1000 * 60 * 60, // 1 hour
+      normalizer: ([uri]: [Uri]) => uri.fsPath,
+    },
+  );
+
+  private memoizedGitIgnore = memoize(
+    async (uri) => {
+      return parseGitIgnore(uri.fsPath);
+    },
+    {
+      max: 100,
+      maxAge: 1000 * 60 * 60 * 24, // 24 hour
+      normalizer: ([uri]: [Uri]) => uri.fsPath,
+    },
+  );
+
+  private findGitRepoUri = async (document: TextDocument): Promise<Uri | null> => {
+    // Try to find git repo by going up the folder tree
+    // Max X folders up to make sure there's no infinite loop
+    let depth = 0;
+    let folderUri = document.uri;
+    do {
+      depth++;
+      folderUri = Uri.file(path.dirname(folderUri.fsPath));
+      const isRepoRoot = await this.memoizedIsRepoRoot(folderUri);
+      if (isRepoRoot) {
+        return folderUri;
+      }
+    } while (folderUri.fsPath !== path.sep && depth < MAX_FOLDER_DEPTH);
+
+    if (depth >= MAX_FOLDER_DEPTH) {
+      // Fallback on gitRepoRoots
+      const gitRepoRoot = Array.from(this.gitRepoRoots).find((root) => document.uri.fsPath.startsWith(root));
+      return gitRepoRoot ? Uri.file(gitRepoRoot) : null;
+    }
+
+    return null;
+  };
 
   get onWatchError() {
     return this.eventEmitter.event;
@@ -37,16 +94,12 @@ class Watcher {
     const client = createClient(refreshToken, this.context);
     const listIds = await getDefaultListIds(client);
 
-    // Parse gitignores
-    const gitIgnores = await Promise.all(this.gitRepoRoots.map(parseGitIgnore));
-    const gitIgnoresByRepoRoots = zipObject(this.gitRepoRoots, gitIgnores);
-
     // List to document saves
-    this.onDidSaveTextDocumentDisposable = workspace.onDidSaveTextDocument((document) => {
-      const repoPath = this.gitRepoRoots.find((root) => document.fileName.startsWith(root));
-      if (repoPath) {
-        const gitIgnore = gitIgnoresByRepoRoots[repoPath];
-        const relativePath = path.relative(repoPath, document.fileName);
+    this.onDidSaveTextDocumentDisposable = workspace.onDidSaveTextDocument(async (document) => {
+      const repoUri = await this.findGitRepoUri(document);
+      if (repoUri) {
+        const gitIgnore = await this.memoizedGitIgnore(repoUri);
+        const relativePath = path.relative(repoUri.fsPath, document.uri.fsPath);
         if (gitIgnore && !gitIgnore.accepts(relativePath)) {
           // File is ignored
           return;
@@ -54,8 +107,8 @@ class Watcher {
       }
 
       updateTodos({
-        filePath: document.fileName,
-        repoPath,
+        filePath: document.uri.fsPath,
+        repoPath: repoUri?.fsPath,
         onCreateTask: async (name) => {
           try {
             return await this.throttle(() => {
